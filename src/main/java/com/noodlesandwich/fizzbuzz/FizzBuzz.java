fef17ea0a39d5449d844738f7395b26d1ab1e1b9
Samir Talwar <samir@noodlesandwich.com>
2013-10-27 18:55:48 +0000

    Add a `main` function to FizzBuzz.

--- src/main/java/com/noodlesandwich/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/noodlesandwich/fizzbuzz/FizzBuzz.java
@@ -13,9 +13,9 @@ public final class FizzBuzz {
     private static final Lambda Five = Succ.call(Succ.call(Succ.call(Succ.call(Succ.call(Zero)))));
     private static final Lambda Fifteen = Multiply.call(Three).call(Five);
 
-    private static final Lambda Fizz = fromList(Arrays.asList(_F, _i, _z, _z));
-    private static final Lambda Buzz = fromList(Arrays.asList(_B, _u, _z, _z));
-    private static final Lambda FizzBuzz = Concat.call(Fizz).call(Buzz);
+    private static final Lambda _Fizz = fromList(Arrays.asList(_F, _i, _z, _z));
+    private static final Lambda _Buzz = fromList(Arrays.asList(_B, _u, _z, _z));
+    private static final Lambda _FizzBuzz = Concat.call(_Fizz).call(_Buzz);
 
     public static List<String> upTo(int max) {
         return toList(upTo(fromInt(max))).stream().map(Characters::toS).collect(Collectors.<String>toList());
@@ -24,16 +24,21 @@ public final class FizzBuzz {
     public static Lambda upTo(Lambda max) {
         return Map.call(i ->
             If.call(IsZero.call(Mod.call(i).call(Fifteen)))
-              .call(FizzBuzz)
+              .call(_FizzBuzz)
               .call(
             If.call(IsZero.call(Mod.call(i).call(Three)))
-              .call(Fizz)
+              .call(_Fizz)
               .call(
             If.call(IsZero.call(Mod.call(i).call(Five)))
-              .call(Buzz)
+              .call(_Buzz)
               .call(
             NumberAsString.call(i)
             )))
         ).call(Range.call(One).call(Succ.call(max)));
+    }
+
+    public static void main(String[] args) {
+        int max = args.length > 0 ? Integer.parseInt(args[0]) : 100;
+        System.out.println(FizzBuzz.upTo(max));
     }
 }
