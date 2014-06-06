140f8b7cdeb2bbed18e3fb053d5ba1ce6ce5c118
Samir Talwar <samir@noodlesandwich.com>
2014-06-07 00:48:30 +0100

    Fix some errors due to ordering.

--- src/main/java/com/noodlesandwich/fizzbuzz/Lambdas.java
+++ src/main/java/com/noodlesandwich/fizzbuzz/Lambdas.java
@@ -14,6 +14,7 @@ public final class Lambdas {
     public static final Lambda Pred = n -> f -> x -> n.call(g -> h -> h.call(g.call(f))).call(ignored -> x).call(u -> u);
     public static final Lambda IsZero = f -> f.call(x -> False).call(True);
 
+    @SuppressWarnings("unused")
     public static final Lambda Y = f -> ((Lambda) (x -> f.call(x.call(x))))
                                                .call(x -> f.call(x.call(x)));
     public static final Lambda Z = f -> ((Lambda) (x -> f.call(y -> x.call(x).call(y))))
@@ -21,6 +22,11 @@ public final class Lambdas {
 
     public static final Lambda Add = x -> x.call(Succ);
     public static final Lambda Subtract = x -> y -> y.call(Pred).call(x);
+
+    public static final Lambda IsLessOrEqual = x -> y -> IsZero.call(Subtract.call(x).call(y));
+
+    public static final Lambda If = p -> t -> f -> p.call(t).call(f);
+
     public static final Lambda Multiply = x -> y -> y.call(Add.call(x)).call(Zero);
     public static final Lambda Divide = n -> d ->
             Z.call(_Divide -> q -> r ->
@@ -33,25 +39,25 @@ public final class Lambdas {
               .call(n -> _Mod.call(Subtract.call(x).call(y)).call(y).call(n))
               .call(x));
 
-    public static final Lambda IsLessOrEqual = x -> y -> IsZero.call(Subtract.call(x).call(y));
-
-    public static final Lambda If = p -> t -> f -> p.call(t).call(f);
-
     public static final Lambda Pair = a -> b -> f -> f.call(a).call(b);
     public static final Lambda First = p -> p.call(a -> b -> a);
     public static final Lambda Second = p -> p.call(a -> b -> b);
 
     public static final Lambda Nil = Pair.call(True).call(True);
     public static final Lambda Cons = h -> t -> Pair.call(False).call(Pair.call(h).call(t));
+    public static final Lambda Head = z -> First.call(Second.call(z));
+    public static final Lambda Tail = z -> Second.call(Second.call(z));
+    public static final Lambda IsNil = First;
+
+    public static final Lambda Fold = Z.call(_Fold -> f -> z -> l ->
+            If.call(IsNil.call(l))
+                    .call(z)
+                    .call(x -> f.call(Head.call(l)).call(_Fold.call(f).call(z).call(Tail.call(l))).call(x)));
     public static final Lambda Append = l -> end -> Fold.call(Cons).call(Cons.call(end).call(Nil)).call(l);
     public static final Lambda Concat = Z.call(_Concat -> a -> b ->
             If.call(IsNil.call(a))
               .call(b)
               .call(Cons.call(Head.call(a)).call(x -> _Concat.call(Tail.call(a)).call(b).call(x))));
-
-    public static final Lambda Head = z -> First.call(Second.call(z));
-    public static final Lambda Tail = z -> Second.call(Second.call(z));
-    public static final Lambda IsNil = First;
     public static final Lambda Index = Z.call(_Index -> i -> list ->
             If.call(IsZero.call(i))
               .call(Head.call(list))
@@ -61,11 +67,6 @@ public final class Lambdas {
             If.call(IsLessOrEqual.call(b).call(a))
               .call(Nil)
               .call(Cons.call(a).call(x -> _Range.call(Succ.call(a)).call(b).call(x))));
-
-    public static final Lambda Fold = Z.call(_Fold -> f -> z -> l ->
-            If.call(IsNil.call(l))
-              .call(z)
-              .call(x -> f.call(Head.call(l)).call(_Fold.call(f).call(z).call(Tail.call(l))).call(x)));
 
     public static final Lambda Map = f -> Fold.call(h -> t -> Cons.call(f.call(h)).call(t)).call(Nil);
 
